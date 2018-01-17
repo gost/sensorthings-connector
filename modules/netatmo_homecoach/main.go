e45936e06a5bd358e706f6cee3ef2b01640d59e0
time <ebben.tim@gmail.com>
2018-01-17 10:18:10 +0100

    Fix homecoach module

--- modules/netatmo_homecoach/main.go
+++ modules/netatmo_homecoach/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gost/sensorthings-connector/module"
-	homecoach "github.com/gost/sensorthings-connector/modules/homecoach/module"
+	homecoach "github.com/gost/sensorthings-connector/modules/netatmo_homecoach/module"
 )
 
 // Module is a mandatory var which is used by the connector
